25b8d1f3bfb2bb7454ff5e73813c73e7129f7a04
qingzhi23 <77131309@qq.com>
2023-08-13 13:52:52 +0800

    提交文件

---
+++ a5/a5/src/london_baseline.py
@@ -0,0 +1,17 @@
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+from tqdm import tqdm
+
+import utils
+
+eval_corpus_path = "birth_dev.tsv"
+eval = open(eval_corpus_path, "r")
+len_eval = len(eval.readlines())
+predictions = ["London"] * len_eval
+total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+if total > 0:
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+else:
+    print('No targets provided!')
